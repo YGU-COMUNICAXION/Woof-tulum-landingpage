181a2029708ca1310530017d491e6e109caabb0d
Emanuel Juarez <emanueljuarez.webdev@gmail.com>
2022-09-26 21:00:39 -0500

    responsive hasta 1500px

--- src/pages/index.js
+++ src/pages/index.js
@@ -100,7 +100,7 @@ const windowSize = useWindowSize();
           <h3>¿Por qué una peluquería canina? </h3>
           <h2 className="tricolor">Bañar, <span>secar,</span> <span1>cortar…</span1></h2>
           <p className="boldP">La peluquería canina es mucho más que el arreglo o la estética del animal.</p>
-          <p className="lightP">En Woof Tulum, te ayudamos a mantener la higiene de tu mascota como prevención de su salud y a detectar alteraciones en su piel, así como alguna enfermedad. También realizamos estas otras tareas.</p>
+          <p className="lightP">En <span>Woof Tulum</span>, te ayudamos a mantener <span>la higiene de tu mascota</span> como <span>prevención de su salud</span> y a <span>detectar alteraciones en su piel</span>, así como alguna enfermedad. También realizamos estas otras tareas.</p>
         </div>
       ):(
         <div className="sobreSecondSectionDesk">
@@ -116,7 +116,7 @@ const windowSize = useWindowSize();
           <h3>¿Por qué una peluquería canina? </h3>
           <h2 className="tricolor">Bañar, <span>secar,</span> <span1>cortar…</span1></h2>
           <p className="boldP">La peluquería canina es mucho más que el arreglo o la estética del animal.</p>
-          <p className="lightP" style={{textAlign : "center"}}>En Woof Tulum, te ayudamos a mantener la higiene de tu mascota como prevención de su salud y a detectar alteraciones en su piel, así como alguna enfermedad. También realizamos estas otras tareas.</p>
+          <p className="lightP" style={{textAlign : "center"}}>En <span>Woof Tulum</span>, te ayudamos a mantener <span>la higiene de tu mascota</span> como <span>prevención de su salud</span> y a <span>detectar alteraciones en su piel</span>, así como alguna enfermedad. También realizamos estas otras tareas.</p>
         </div>
       )}
         {windowSize <  752 ? (
@@ -373,11 +373,11 @@ const windowSize = useWindowSize();
           <>
             <div className="contactoDeskDiv">
               <div className="contactoCardsDivDesk">
-              <ContactoCard icon={Phone} title={"Teléfono"} info={"+52 984 217 0782"}/>
-              <ContactoCard icon={Clock} title={"Horario"} info={"Lunes a Viernes: 9:00am - 18:00pm"}>
-                <p>Sábados: 9:00am - 17:00pm</p>
-              </ContactoCard>
-              <ContactoCard icon={Ubi} title={"Dirección"} info={"Av. La Selva, 77760 Tulum, Q.R., México"}/>
+                <ContactoCard icon={Phone} title={"Teléfono"} info={"+52 984 217 0782"}/>
+                <ContactoCard icon={Clock} title={"Horario"} info={"Lunes a Viernes: 9:00am - 18:00pm"}>
+                  <p>Sábados: 9:00am - 17:00pm</p>
+                </ContactoCard>
+                <ContactoCard icon={Ubi} title={"Dirección"} info={"Av. La Selva, 77760 Tulum, Q.R., México"}/>
               </div>
               <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14975.835857120077!2d-87.4692647!3d20.2190262!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x42b4fe450d31e588!2sWoof%20Tulum!5e0!3m2!1ses!2smx!4v1664055019260!5m2!1ses!2smx" className="mapa" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
