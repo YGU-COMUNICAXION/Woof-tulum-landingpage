8bd0e94abc0635a2e9fab3d1e419578482f030d6
Jonathan Bello <jonathan.bellofg@gmail.com>
2022-10-06 17:35:00 -0500

    traduccion de Appointment

--- src/components/pages/home/AppointmentWoof.jsx
+++ src/components/pages/home/AppointmentWoof.jsx
@@ -7,6 +7,7 @@ import WhatsappButton from "../../../images/mobile/cita/WhatsappLogo.svg";
 import BurbujasRojas from "../../../images/desktop/layout/circulos-coral.svg";
 import useWindowSize from "../../../hooks/useWindowSize";
 import { useTranslation } from "gatsby-plugin-react-i18next";
+import renderStringHMTLtoJSX from "../../../utils/renderStringHMTLtoJSX";
 
 const AppointmentWoof = () => {
   const windowSize = useWindowSize();
@@ -20,30 +21,22 @@ const AppointmentWoof = () => {
           <div id="cita" className="citaFirstSection">
             <img src={HuellasA} />
             <div className="hazCitaDiv">
-              <h2>Haz tu cita</h2>
+              <h2>{t("AppointmentTitle")}</h2>
               <p className="hazCitaFirstP">
-                Comprueba nuestra <span>disponibilidad</span> y{" "}
-                <span>reserva la fecha y la hora</span> que más te convengan. Te
-                sugerimos <span>agendar</span> con al menos{" "}
-                <span>1 día de anticipación</span> para garantizar que tengamos
-                espacio disponible para tu mascota.
+                {renderStringHMTLtoJSX(t("AppointmentParam1"))}
               </p>
-              <p className="hazCitaSecondP">
-                *En caso de que tu mascota venga con pulgas y/o garrapatas
-                considera que se aplicará un tarifa extra a tu servicio.
-              </p>
+              <p className="hazCitaSecondP">{t("AppointmentParam2")}</p>
               <Boton url={"https://wa.me/529842170782"}>
                 <img src={WhatsappButton} />
-                <p>Agendar cita</p>
+                <p>{t("ButtonText")}</p>
               </Boton>
             </div>
           </div>
           <div className="citaSecondSection">
             <div className="regaloDiv">
-              <h2>¡Regalo para tu peludo!</h2>
+              <h2>{t("AppointmentTitle2")}</h2>
               <p className="regaloP">
-                Por ser <span>cliente frecuente</span>, el{" "}
-                <span>10mo baño</span> va por nuestra cuenta.{" "}
+                {renderStringHMTLtoJSX(t("AppointmentParam3"))}
               </p>
             </div>
           </div>
@@ -53,22 +46,15 @@ const AppointmentWoof = () => {
           <div id="cita" className="citaFirstSectionDesk">
             <div className="citaUpperDiv">
               <div className="hazCitaDivDesk">
-                <h2>Haz tu cita</h2>
+                <h2>{t("AppointmentTitle")}</h2>
                 <p className="hazCitaFirstP">
-                  Comprueba nuestra <span>disponibilidad</span> y{" "}
-                  <span>reserva la fecha y la hora</span> que más te convengan.
-                  Te sugerimos <span>agendar</span> con al menos{" "}
-                  <span>1 día de anticipación</span> para garantizar que
-                  tengamos espacio disponible para tu mascota.
+                  {renderStringHMTLtoJSX(t("AppointmentParam1"))}
                 </p>
-                <p className="hazCitaSecondP">
-                  *En caso de que tu mascota venga con pulgas y/o garrapatas
-                  considera que se aplicará un tarifa extra a tu servicio.
-                </p>
+                <p className="hazCitaSecondP">{t("AppointmentParam2")}</p>
                 <div className="citabotonDivDesk">
                   <Boton url={"https://wa.me/529842170782"}>
                     <img src={WhatsappButton} />
-                    <p>Agendar cita</p>
+                    <p>{t("ButtonText")}</p>
                   </Boton>
                 </div>
               </div>
@@ -76,10 +62,9 @@ const AppointmentWoof = () => {
             </div>
             <div className="regaloDivDesk">
               <div className="regaloDivInfo">
-                <h2>¡Regalo para tu peludo!</h2>
+                <h2>{t("AppointmentTitle2")}</h2>
                 <p className="regaloPDesk">
-                  Por ser <span>cliente frecuente</span>, el{" "}
-                  <span>10mo baño</span> va por nuestra cuenta.{" "}
+                  {renderStringHMTLtoJSX(t("AppointmentParam3"))}
                 </p>
               </div>
             </div>
