26148be8dff9a330469ca0f58bcd324910426105
Jonathan Bello <jonathan.bellofg@gmail.com>
2022-10-06 18:04:47 -0500

    traduccion de testimonialsWoof

--- src/components/pages/home/TestimonialsWoof.jsx
+++ src/components/pages/home/TestimonialsWoof.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { useTranslation } from "gatsby-plugin-react-i18next";
 import CarouselTestimonios from "../../../components/carousel";
 import useWindowSize from "../../../hooks/useWindowSize";
 import PeritoTestimonios from "../../../images/desktop/testimonios/foto (1).svg";
 
 const TestimonialsWoof = () => {
   const windowSize = useWindowSize();
+  const { t } = useTranslation("testimonialsWoof");
 
   return (
     <div id="testimonios" className="testimonios">
-      <h1 className="sectionTitle">Testimonios</h1>
+      <h1 className="sectionTitle">{t("TestimonialTitle")}</h1>
       {windowSize < 752 ? (
         <div className="carouselDiv">
           <CarouselTestimonios />
